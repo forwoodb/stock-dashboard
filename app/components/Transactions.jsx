9dafacd13b3cc53463b075d0d7196b7d8442ccae
Brenton Forwood <forwoodb77@gmail.com>
2026-04-03 06:51:38 -0400

    Created edit functionality for trades

--- app/components/Transactions.jsx
+++ app/components/Transactions.jsx
@@ -1,65 +1,76 @@
 "use client";
+import { useState } from "react";
 import Button from "../components/Button";
+import TradeForm from "./TradeForm";
 
 const Transactions = ({ trades, deleteTrade }) => {
-  const editTrade = (id) => {
-    console.log(id);
+  const [edit, setEdit] = useState(false);
+  const [editStock, setEditStock] = useState({});
+
+  const editTrade = (item) => {
+    console.log(item);
+    setEdit(true);
+    setEditStock(item);
   };
 
-  return (
-    <div>
-      <h1>Transactions</h1>
-      <table className="w-[95%] m-auto">
-        <thead>
-          <tr>
-            <th>Ticker</th>
-            <th>PosSize</th>
-            <th>Price</th>
-            <th>Type</th>
-            <th>5D</th>
-            <th>10D</th>
-            <th>20D</th>
-            <th>50D</th>
-            <th>100D</th>
-            <th>200D</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {trades.map((trade) => {
-            return (
-              <tr key={trade._id}>
-                <td>{trade.ticker}</td>
-                <td>{trade.positionSize}</td>
-                <td>{trade.price}</td>
-                <td className="capitalize">{trade.type}</td>
-                <td>{trade.fiveDayAvg}</td>
-                <td>{trade.tenDayAvg}</td>
-                <td>{trade.twentyDayAvg}</td>
-                <td>{trade.fiftyDayAvg}</td>
-                <td>{trade.oneHundredDayAvg}</td>
-                <td>{trade.twoHundredDayAvg}</td>
-                <td>
-                  <Button click={() => editTrade(trade._id)} border>
-                    Edit
-                  </Button>
-                </td>
-                <td>
-                  <Button
-                    click={() => deleteTrade(trade._id)}
-                    className="bg-red-600 text-white"
-                  >
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+  if (edit) {
+    return <TradeForm stock={editStock} />;
+  } else {
+    return (
+      <div>
+        <h1>Transactions</h1>
+        <table className="w-[95%] m-auto">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>PosSize</th>
+              <th>Price</th>
+              <th>Type</th>
+              <th>5D</th>
+              <th>10D</th>
+              <th>20D</th>
+              <th>50D</th>
+              <th>100D</th>
+              <th>200D</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {trades.map((trade) => {
+              return (
+                <tr key={trade._id}>
+                  <td>{trade.ticker}</td>
+                  <td>{trade.positionSize}</td>
+                  <td>{trade.price}</td>
+                  <td className="capitalize">{trade.type}</td>
+                  <td>{trade.fiveDayAvg}</td>
+                  <td>{trade.tenDayAvg}</td>
+                  <td>{trade.twentyDayAvg}</td>
+                  <td>{trade.fiftyDayAvg}</td>
+                  <td>{trade.oneHundredDayAvg}</td>
+                  <td>{trade.twoHundredDayAvg}</td>
+                  <td>
+                    <Button click={() => editTrade(trade)} border>
+                      Edit
+                    </Button>
+                  </td>
+                  <td>
+                    <Button
+                      click={() => deleteTrade(trade._id)}
+                      className="bg-red-600 text-white"
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 
 export default Transactions;
